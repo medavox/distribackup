09bd7e7808190f3009dbe1bf4dc1ed595b9c8fce
unknown <howarda3@EGB074000013.lancs.local>
2014-11-06 13:37:22 +0000

    made some doc filenames more windows-friendly, and better practice

--- src/Publisher.java
+++ src/Publisher.java
@@ -3,11 +3,14 @@ import java.util.*;
 
 public class Publisher
 {
+	private final int LISTEN_SOCKET_MAX_BACKLOG = 50;
+	private final int LISTEN_PORT = 1210;
     private List<Peer> peers = new LinkedList<Peer>();
     
     public Publisher()
     {
-        
+        ServerSocket svr = new ServerSocket(LISTENING_PORT, LISTEN_SOCKET_MAX_BACKLOG);
+		
     }
     
     public static void main (String args[])
