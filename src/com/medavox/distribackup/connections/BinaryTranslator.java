ecdc65f187c71e7fada6529e74db478e7e85c7f5
Adam Howard <medavox2+github@gmail.com>
2014-12-28 18:38:39 +0000

    implementing binary conversion methods in BinaryTranslator; added notes about how to handle incoming messages from multiple connection

--- src/com/medavox/distribackup/connections/BinaryTranslator.java
+++ src/com/medavox/distribackup/connections/BinaryTranslator.java
@@ -2,72 +2,181 @@ package com.medavox.distribackup.connections;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.nio.file.Path;
+import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
 import java.io.DataOutputStream;
 import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.EOFException;
+
+
+import java.nio.file.Path;
 
 import com.medavox.distribackup.peers.PeerInfo;
 
 /**Try to keep logic in this class just pertaining to conversion to and from binary*/
 public class BinaryTranslator
 {
-    DataOutputStream dos;
-    DataInputStream dis;
-    ByteArrayOutputStream ba = new ByteArrayOutputStream();
+	/*BITFIELD		((byte)0x00,  2),
+	STRING			((byte)0x01, -1),
+	UBYTENUM		((byte)0x02,  2),
+	USHORT			((byte)0x03,  3),
+	UINTEGER		((byte)0x04,  5),
+	ULONG			((byte)0x05,  9),
+	BYTENUM			((byte)0x06,  2),
+	SHORT			((byte)0x07,  3),
+	INTEGER			((byte)0x08,  5),
+	LONG			((byte)0x09,  9),*/
+	
+	/**TODO:
+	ubyte  -> byte
+	byte   -> byte
+	ushort -> bytes
+	short  -> bytes    DONE
+	uint   -> bytes
+	int    -> bytes    DONE
+	ulong  -> bytes
+	long   -> bytes    DONE
+	 
+	byte  -> ubyte
+	byte  -> byte
+	bytes -> ushort
+	bytes -> shortm
+	bytes -> uint
+	bytes -> int
+	bytes -> ulong
+	bytes -> long      DONE
+	
+	
+	bitfield		-> byte
+	byte			-> bitfield
+	
+	String			-> bytes
+	bytes			-> String
+
+	FileInfo	    -> bytes
+	bytes       	-> FileInfo
+	 
+	DirectoryInfo   -> bytes
+	bytes           -> DirectoryInfo
+	
+	PeerInfo    	-> bytes
+	byte			-> PeerInfo
+	
+	 */
+	static DataOutputStream dos;
+	//DataInputStream dis;
+	static ByteArrayOutputStream ba = new ByteArrayOutputStream();
+	//ByteArrayInputStream bais = new ByteArrayInputStream();
 	public BinaryTranslator()
 	{
 		dos = new DataOutputStream(ba);
 	}
 	
 
-    public static byte[] shortToBytes(short l)
-    {
-        ba.reset();//reset byte output buffer
-        dos.writeShort(l);
-        return ba.toByteArray();
-    }
-    
-    public static byte[] intToBytes(int l)
-    {
-        ba.reset();//reset byte output buffer
-        dos.writeInt(l);
-        return ba.toByteArray();
-    }
-    
-    public static byte[] longToBytes(long l)
-    {
-        ba.reset();//reset byte output buffer
-        dos.writeLong(l);
-        return ba.toByteArray();
-    }
-    
-    public static long bytesToLong(byte[] b) throws IOException, EOFException
-    {
-        
-    }
-    
-    public static byte[] concat(byte[]... bytes)
-    {
-        //count how many bytes in total we have
-        int count = 0;
-        for(int i = 0; i < bytes.length; i++)
-        {
-            for(int j = 0; j < bytes[i].length; j++)
-            {
-                count++;
-            }
-        }
-        //write every byte in argument arrays into ret, in order
-        byte[] ret = new byte[count];
-        int index = 0;
-        for(int i = 0; i < bytes.length; i++)
-        {
-            for(int j = 0; j < bytes[i].length; j++)
-            {
-                ret[index] = bytes[i][j];
-                index++;
-            }
-        }
-        return ret;
-    }
+	public static byte[] shortToBytes(short l) throws IOException
+	{
+		ba.reset();//reset byte output buffer
+		dos.writeShort(l);
+		return ba.toByteArray();
+	}
+	
+	public static byte[] intToBytes(int l) throws IOException
+	{
+		ba.reset();//reset byte output buffer
+		dos.writeInt(l);
+		return ba.toByteArray();
+	}
+	
+	public static byte[] longToBytes(long l) throws IOException
+	{
+		ba.reset();//reset byte output buffer
+		dos.writeLong(l);
+		return ba.toByteArray();
+	}
+	
+	public static long bytesToLong(byte[] b) throws IOException, EOFException
+	{
+		
+		DataInputStream dis = new DataInputStream(new ByteArrayInputStream(b) );
+		return dis.readLong();
+	}
+	
+	public static byte bitfieldToByte(boolean... bools) throws Exception
+	{
+		byte out = (byte)0;
+		if(bools.length > 8)
+		{
+			throw new Exception("Too many booleans to fit inside a byte!"); 
+		}
+		else
+		{
+			for(int i = 0; i < bools.length; i++)
+			{//fill fields up from lsb first
+				if(bools[i])
+				{
+					byte mask = (byte)(0x1 << i);
+					out &= mask;
+				}
+			}
+		}
+		return out;
+	}
+	
+	public static byte[] generateFileInfoBytes(Path p)
+	{
+		/*
+		0. (ID Byte)        | a byte
+		0. (Length)         | UInteger
+		1. Name             | String
+		2. Path             | String
+		3. file size        | ULong
+		4. revision number  | ULong
+		5. checksum         | SHA1*/
+		
+		byte byteID = Message.FILE_INFO.IDByte;
+		long length 
+	}
+	
+	public static boolean[] byteToBitfield(byte in, byte numFields)
+	{
+		if(numFields > 8)
+		{
+			System.err.println("WARNING: number of requested fields from a byte was >8!\nassuming 8 fields.");
+			numFields = 8;
+		}
+		boolean[] fields = new boolean[numFields];
+		
+		for(int i = 0; i < numFields; i++)
+		{
+			byte mask = (byte)(0x1 << i);
+			byte test = in & mask;
+			fields[i] = (test > 0);
+		}
+	}
+	
+	public static byte[] concat(byte[]... bytes)
+	{
+		//count how many bytes in total we have
+		int count = 0;
+		for(int i = 0; i < bytes.length; i++)
+		{
+			for(int j = 0; j < bytes[i].length; j++)
+			{
+				count++;
+			}
+		}
+		//write every byte in argument arrays into ret, in order
+		byte[] ret = new byte[count];
+		int index = 0;
+		for(int i = 0; i < bytes.length; i++)
+		{
+			for(int j = 0; j < bytes[i].length; j++)
+			{
+				ret[index] = bytes[i][j];
+				index++;
+			}
+		}
+		return ret;
+	}
 }
--- src/com/medavox/distribackup/connections/ConnectionOperator.java
+++ src/com/medavox/distribackup/connections/ConnectionOperator.java
@@ -13,94 +13,119 @@ import com.medavox.distribackup.peers.PeerInfo;
 /**This class handles communication over an individual Socket connection, after its initialisation. */
 public class ConnectionOperator extends Thread
 {
+        
+        /*ADDRESS               ((byte)0x0A, -2),
+        BYTE_ARRAY              ((byte)0x0B, -1),
+        PEER_INFO               ((byte)0x0C, -2),
+        DIRECTORY_INFO  ((byte)0x0D, -2),
+        LIST                    ((byte)0x0E, -1),
+        FILE_INFO               ((byte)0x0F, -2),
+        FILEDATA                ((byte)0x12, -2),
+        FILE_REQUEST    ((byte)0x13, -2),
+        GREETING                ((byte)0x14, 18),
+        TREE_STATUS_REQ ((byte)0x16,  0),
+        UPDATE_ANNOUNCE ((byte)0x17, -2),
+        HLIST                   ((byte)0x18, -1);*/
     BufferedInputStream bis;
     BufferedOutputStream bos;
     Socket socket;
-	public ConnectionOperator(Socket s/*, PeerInfo*/) throws IOException
-	{
-	    this.socket = s;
-	    bis = new BufferedInputStream(s.getInputStream());
-	    bos = new BufferedOutputStream(s.getOutputStream());
-	}
+        public ConnectionOperator(Socket s/*, PeerInfo*/) throws IOException
+        {
+            this.socket = s;
+            bis = new BufferedInputStream(s.getInputStream());
+            bos = new BufferedOutputStream(s.getOutputStream());
+        }
     
     /**Checks that local and remote program versions match*/
     public int checkVersions()
         throws IOException
     {
-		//see if we've seen the connecting peer before
-		//if not, create a new PeerInfo object (query for some info)
-		//if we have, then add this socket to that peer's open sockets
-		short version = Peer.version;
-		//construct magic number and version number byte array
-		byte[] ret = new byte[2];
-		//short -> bytes
-		ret[1] = (byte)(version & 0xff);
-		ret[0] = (byte)(version >> 8);
-		
-		//"Be Selfless" + version:short
-		byte[] versionBytes = {(byte)0xBE, (byte)0x5E, (byte)0x1F, (byte)0x1E, (byte)0x55, ret[0], ret[1]};
-		
-		bos.write(versionBytes, 0, versionBytes.length);//send our version string
-		bos.flush();
-		
-		//read their version string
-		byte[] theirVersion = new byte[versionBytes.length];
-		bis.read(theirVersion, 0, theirVersion.length);
-		
-		//their version bytes -> short
-		short theirVer = (short) (theirVersion[theirVersion.length-1] | (theirVersion[theirVersion.length-2] << 8));
+	//see if we've seen the connecting peer before
+	//if not, create a new PeerInfo object (query for some info)
+	//if we have, then add this socket to that peer's open sockets
+	short version = Peer.version;
+	//construct magic number and version number byte array
+	byte[] ret = new byte[2];
+	//short -> bytes
+	ret[1] = (byte)(version & 0xff);
+	ret[0] = (byte)(version >> 8);
+	
+	//"Be Selfless" + version:short
+	byte[] versionBytes = {(byte)0xBE, (byte)0x5E, (byte)0x1F, (byte)0x1E, (byte)0x55, ret[0], ret[1]};
+	
+	bos.write(versionBytes, 0, versionBytes.length);//send our version string
+	bos.flush();
+	
+	//read their version string
+	byte[] theirVersion = new byte[versionBytes.length];
+	bis.read(theirVersion, 0, theirVersion.length);
+	
+	//their version bytes -> short
+	short theirVer = (short) (theirVersion[theirVersion.length-1] | (theirVersion[theirVersion.length-2] << 8));
 
-		System.out.println("Our version:   "+version);
-		System.out.println("Their version: "+theirVer);
-		if(version != theirVer)
-		{
-		    return -1;
-		}
-		return 0;
+	System.out.println("Our version:   "+version);
+	System.out.println("Their version: "+theirVer);
+	if(version != theirVer)
+	{
+	    return -1;
+	}
+	return 0;
     }
     
-	/*SENDING:Methods to send data down the socket*/
+        /*SENDING:Methods to send data down the socket*/
     
     /**Sends a greeting (containing our UUID), waits for a greeting in reply, and returns the received UUID */
-	UUID greeting()
-	{
+    UUID greeting()
+    {
         //package up data into a single bytestream before sending, 
         //(as opposed to sending each bit as we create it), to minimise packets
         byte[] UUIDmsb = BinaryTranslator.longToBytes(Peer.myUUID.getMostSignificantBits());
         byte[] UUIDlsb = BinaryTranslator.longToBytes(Peer.myUUID.getLeastSignificantBits());
-		byte[] greetingSend = BinaryTranslator.concat(Message.GREETING, UUIDmsb, UUIDlsb);
+	byte[] greetingSend = BinaryTranslator.concat(Message.GREETING.IDByte, UUIDmsb, UUIDlsb);
         bos.write(greetingSend);
         bos.flush();
         
         //wait for greeting back
         byte[] theirGreeting = new byte[Message.GREETING.length-2];//minus header
-		bis.read(theirGreeting, 2, theirGreeting.length);//again, skip header
-	}
+	bis.read(theirGreeting, 2, theirGreeting.length);//again, skip header
+                
+	long theirUUIDmsb = BinaryTranslator.bytesToLong(Arrays.copyOfRange(theirGreeting, 0, 7));
+	long theirUUIDlsb = BinaryTranslator.bytesToLong(Arrays.copyOfRange(theirGreeting, 8, 15));
 	
-	void announceExit()
-	{
-		
-	}
+	return new UUID(theirUUIDmsb, theirUUIDlsb);
+    }
+        
+    void announceExit()
+    {
+	bos.write(Message.EXIT_ANNOUNCE.IDByte, 0, 1);
+	bos.flush();
+    }
+    
+    void requestMorePeers(PeerInfo[] knownPeers)
+    {
+	bos.write(Message.REQ_FOR_PEERS.IDByte, 0, 1);
+	bos.flush();
+    }
+    
+    void sendPeerInfo(PeerInfo[] peers)
+    {
 	
-	void requestMorePeers(PeerInfo[] knownPeers)
-	{
-		
-	}
+    }
+    
+    void sendTreeStatus()
+    {
 	
-	void sendPeerInfo(PeerInfo[] peers)
-	{
-		
-	}
+    }
+    
+    void sendFileData()
+    {
 	
-	void sendTreeStatus()
-	{
-		
-	}
-	
+    }
+        
     void requestFile(Path p)
-	{
-		
-	}
+    {
+	
+    }
     /*RECEIVER: Handling incoming data*/
     public void run()
     {
@@ -108,16 +133,16 @@ public class ConnectionOperator extends Thread
     }
     
     public void close() throws IOException
-	{
-		try
-	    {
+    {
+        try
+        {
             bis.close();
             bos.close();
             socket.close();
-	    }
-	    catch(IOException ioe)
-	    {
+        }
+        catch(IOException ioe)
+        {
             System.err.println("Unable to close Connection!");
-	    }
-	}
+        }
+    }
 }
