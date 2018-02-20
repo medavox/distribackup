f3841bb37ef80ad52e702a133d5a732215946dbf
Adam Howard <achdiposable1@gmail.com>
2018-02-20 10:15:23 +0000

    add helper function to HashingWrapper for hashing file contents

--- src/main/java/com/medavox/distribackup/hashing/HashingWrapper.java
+++ src/main/java/com/medavox/distribackup/hashing/HashingWrapper.java
@@ -1,7 +1,16 @@
 package com.medavox.distribackup.hashing;
+
+import okio.BufferedSource;
+import okio.Okio;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
 /**Abstracts away implementations of our hashing function,
  * so we can easily test and swap out different hashing algorithms*/
 public abstract class HashingWrapper {
+    public static final int FILE_HASHING_BUFFER_SIZE = 1048576;//1MiB
 
     public abstract void addBytes(byte[] data);
     public abstract byte[] computeHash();
@@ -12,5 +21,20 @@ public abstract class HashingWrapper {
             instance = new JavaHashing(JavaHashing.Algorithm.SHA256);
         }
         return instance;
+    }
+
+    public static byte[] hashFile(File fileToAdd) throws IOException {
+        byte[] buffer = new byte[FILE_HASHING_BUFFER_SIZE];
+        HashingWrapper hasher = HashingWrapper.get();
+        //FileInputStream doesn't cut it here, it only supports files of size <2GiB
+        BufferedSource bufferedSource = Okio.buffer(Okio.source(fileToAdd));
+        int bytesRead = bufferedSource.read(buffer);
+        while(bytesRead == buffer.length) {
+            hasher.addBytes(buffer);
+            bytesRead = bufferedSource.read(buffer);
+        }
+        hasher.addBytes(Arrays.copyOfRange(buffer, 0, bytesRead));
+        bufferedSource.close();
+        return hasher.computeHash();
     }
 }
