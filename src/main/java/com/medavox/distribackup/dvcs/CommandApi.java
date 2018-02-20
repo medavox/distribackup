db23c0520330528f9d643b5786904252e81a5bb3
Adam Howard <achdiposable1@gmail.com>
2018-02-20 10:15:57 +0000

    begin work on external api methods for CLI

---
+++ src/main/java/com/medavox/distribackup/dvcs/CommandApi.java
@@ -0,0 +1,52 @@
+package com.medavox.distribackup.dvcs;
+
+import com.medavox.distribackup.hashing.HashingWrapper;
+import okio.BufferedSource;
+import okio.Okio;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class CommandApi {
+    public static final int FILE_HASHING_BUFFER_SIZE = 1048576;//1MiB
+
+    /**
+    1. Write the output of diff /dev/null <file> to a file named with the hash of the file's contents,
+    stored in ./.distribackup
+
+    2. Write a copy of the file itself to ./.distribackup/latest-edition/<path>/<file>
+
+    3. Write the list of hashes that changed in this edition, along
+    */
+    public static void addCommand(File fileToAdd) {
+        /*todo:
+        perform centralised validation on the file object before it reaches this method:
+        file exists
+        is a file
+        if an absolute path, path referenced is inside the repository directory*/
+        Thread t = new Thread(new Runnable() {
+            @Override public void run() {
+                try {
+                    HashingWrapper.hashFile(fileToAdd);
+                }catch(IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
+        }, "hashing file \""+fileToAdd.getName()+"\"");
+        t.start();
+    }
+
+
+    /*Update <file>:
+    *
+    * */
+
+    /*Remove <file>:
+    * */
+
+    /*If we automcatically publish editions on every file change,
+    then we may want to give users the ability to simplify down the edition history;
+    to remove redundant intermediate editions before sending across the network*/
+}
